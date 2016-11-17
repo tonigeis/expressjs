ebe49697d0479d4f62d2292b24dbfe111a838be1
tonigeis <antoniogeisinfiesta@gmail.com>
2016-11-17 17:21:56 +0100

    Formulario de contacto

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-//El index.js actua de controlador 
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
 var things = require('./things.js'); 
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+//app.use(upload.array()); // for parsing multipart/form-data
+
+app.use(express.static('public'));
+app.use('/js', express.static('js'));
+app.use('/img', express.static('img'));
+
+//Simple request time logger
+app.use('/hotel', function(req, res, next){
+	console.log("A new request received at " + Date.now());
+	//This function call is very important. It tells that more processing is 
+	//required for the current request and is in the next middleware function/route handler.
+	next();
+});
+
 //both index.js and things.js should be in same directory
 app.use('/', things); 
 
--- things.js
+++ things.js
@@ -2,15 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.send('Hola mundo!! Express!!');
+	var drinks = [
+	    { name: 'Bloody Mary', drunkness: 3 },
+	    { name: 'Martini', drunkness: 5 },
+	    { name: 'Scotch', drunkness: 10 }
+	];
+  	res.render('home', {
+        name: "TutorialsPoint", 
+        url:"http://www.tutorialspoint.com",
+        drinks: drinks
+  });
 });
 
-router.get('/hello', function(req, res) {
-  res.send('Hello world!!');
+router.get('/hello', function(req, res){
+	res.send("Hello World!");
 });
 
-router.post('/hello', function(req, res) {
-  res.send('POST - Hello world!!');
+router.post('/hello', function(req, res){
+	res.send("You just called the post method at '/hello'!\n");
 });
 
 router.get('/bienvenido/:nombre', function(req, res) {
@@ -18,12 +27,26 @@ router.get('/bienvenido/:nombre', function(req, res) {
 });
 
 router.get('/hotel/:id', function(req, res){
-	//Obtengo datos del hotel id en BBDD
-    res.send('El hotel con id ' + req.params.id + ' tiene estas caracteristicas');
+	//Obtengo datos del Hotel id en BBDD
+    res.send('El hotel ' + req.params.id + ' tiene estas caractersticas...' );
 });
 
 router.get('/things/:id([0-9]{5})', function(req, res){
     res.send('id: ' + req.params.id);
+});
+
+router.get('/contacto', function(req, res){
+	res.render('form.ejs');
+});
+
+router.post('/contacto', function(req, res){
+	console.log(req.body);
+	console.log(req.body.say);
+    res.send("recieved your request!");
+});
+
+router.get('*', function(req, res){
+    res.send('Lo siento, no has acertado.');
 });
 
 //export this router to use in our index.js
